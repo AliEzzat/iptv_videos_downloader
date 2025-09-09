797c3f122ea031d55e810b39ef5a22b55c4e6ac9
Ali Ezzat <aliezat402@gmail.com>
2025-09-09 03:10:35 +0300

    mimic login

--- api/proxy.js
+++ api/proxy.js
@@ -13,7 +13,36 @@ export default async function handler(req, res) {
   try {
     targetUrl = decodeURIComponent(rawUrl);
     if(targetUrl.includes("get_account_info")){
-      return res.status(200).json({ status: 'ok', data: {"username":"aliezzat","password":"36622511","message":"","auth":1,"status":"Active","exp_date":"1785793040","is_trial":"0","active_cons":"1","created_at":"1722708363","max_connections":"1","allowed_output_formats":["m3u8","ts","rtmp"]},"server_info":{"url":"lionzhd.com","port":"2095","https_port":"2096","server_protocol":"http","rtmp_port":"25462","timezone":"Asia\/Kuwait","timestamp_now":1757373753,"time_now":"2025-09-09 02:22:33","process":true}}); 
+      return res.status(200).json({ status: 'ok', data: {
+  "user_info": {
+    "username": "aliezzat",
+    "password": "36622511",
+    "message": "",
+    "auth": 1,
+    "status": "Active",
+    "exp_date": "1785793040",
+    "is_trial": "0",
+    "active_cons": "0",
+    "created_at": "1722708363",
+    "max_connections": "1",
+    "allowed_output_formats": [
+      "m3u8",
+      "ts",
+      "rtmp"
+    ]
+  },
+  "server_info": {
+    "url": "lionzhd.com",
+    "port": "2095",
+    "https_port": "2096",
+    "server_protocol": "http",
+    "rtmp_port": "25462",
+    "timezone": "Asia/Kuwait",
+    "timestamp_now": 1757376544,
+    "time_now": "2025-09-09 03:09:04",
+    "process": true
+  }
+}}); 
     }
     const urlObj = new URL(targetUrl);
     const hostPort = `${urlObj.hostname}:${urlObj.port}`;
